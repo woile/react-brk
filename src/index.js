6ff2caaeae7783cf9934bc9aa80d4a60ef4d7957
santiago fraire <santiwilly@gmail.com>
2018-02-15 21:41:18 +0100

    refactor: removed unused code

--- src/index.js
+++ src/index.js
@@ -6,13 +6,3 @@ export const Desktop = props => <Responsive {...props} minWidth={992} />
 export const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />
 export const Mobile = props => <Responsive {...props} maxWidth={767} />
 export const Default = props => <Responsive {...props} minWidth={768} />
-
-
-const breakpoints = () => {
-  const custom = {}
-  return {
-    register: () => {
-
-    }
-  }
-}
